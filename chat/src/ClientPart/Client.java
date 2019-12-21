83670c003f8db459d8bda4dcbd343c82b175d7e3
Вера <o.vorona6@yandex.ru>
2019-12-22 00:28:44 +0700

    код исправлен и дополнен

--- chat/src/ClientPart/Client.java
+++ chat/src/ClientPart/Client.java
@@ -1,76 +1,53 @@
 package ClientPart;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
+
 
 // клиентская часть
 class Client {
     public static void main(String[] args) throws Exception {
-        int portNumber = 1777; //определяем номер порта на котором нас ожидает сервер для ответа
-//мои изиенения 3
-        String name_i;
-        String messager;
-// закончились мои изменения 3
-        //      String messager = "тестовая строка"; // подготавливаем строку для запроса
-        System.out.println("Клиент стартовал");//пишем что стартовал клиент
-        Socket socket = new Socket("127.0.0.1", portNumber);//открыть сокет для обращения к локальному компьютеру
+        //определяем номер порта на котором нас ожидает сервер для ответа
+        int portNumber = 1777;
+        //открыть сокет для обращения к локальному компьютеру
+        Socket socket = new Socket("127.0.0.1", portNumber);
         //создаём поток для чтения символов из сокета.
         // для этого открывается поток сокета - socket.getInputStream()
         // потом преобразовываем его в поток символов - new InputStreamReader
         // делаем его читателем строк - BufferedReader
-        BufferedReader br= new BufferedReader (new InputStreamReader(socket.getInputStream()));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         //создаём поток для записей символов в сокет
         PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-        //определяем тестовую строку в сокет
-// далее мои изменения, до них всё работало
-        //     Scanner scan = new Scanner(System.in);
-        //здесь пошли изменения 2
-        //    name_i=scan.nextLine();
-        //    pw.println(name_i);
-//////////////////////////////////////////////////////////////////////////////////////////////// далее всё ушло в do while
-      /* Scanner scan2=new Scanner(System.in);
-  //закончились изменения 2
-        messager = scan2.nextLine(); // подготавливаем строку для запроса
-// закончились изменения
 
-       pw.println(messager);*/
-        //выводим в цикл чтения, что нам ответил сервер
-/////////////////////////////////////////////////////////////////////////////// мой блок while
+        System.out.print("Введите имя: ");
+        Scanner nam = new Scanner(System.in);
+        String name = nam.nextLine();
+        pw.println(name);
 
-        do {
-            Scanner scan2 = new Scanner(System.in);
-            //закончились изменения 2
-            messager = scan2.nextLine(); // подготавливаем строку для запроса
-// закончились изменения
 
+        System.out.println("Клиент " + name + " стартовал");//пишем что стартовал клиент
+        System.out.print(">>");
+
+        Scanner mes = new Scanner(System.in);
+        String messager = mes.nextLine();
+
+        pw.println(messager);
+
+
+        while (!messager.equals("Пока")) {
+            System.out.print(">>");
+
+            messager = mes.nextLine();
             pw.println(messager);
-            if (messager.equals("Пока")) {
-                break;
-            }
-// печатаем ответ от сервера на консоль для проверки
-            //  System.out.println(messager);  //не печатаем сообщение
-            //   pw.println("bye");
-        }while ((messager = br.readLine()) !=null);
-
-        /*while ((messager = br.readLine()) !=null) {
-            //если пришёл ответ "bye", то заканчиваем цикл
-            ///////////////////////////////////////////////////////////////////////
+        }
 
 
-            if (messager.equals("bye")){
-                break;
-            }
-            // печатаем ответ от сервера на консоль для проверки
-            System.out.println(messager);
-         //   pw.println("bye");
-        }*/
         br.close();
         pw.close();
         socket.close();
     }
 }
-
--- chat/src/ServerPart/ThreadServer.java
+++ chat/src/ServerPart/ThreadServer.java
@@ -1,13 +1,27 @@
 package ServerPart;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
 
 // серверная часть
 public class ThreadServer {
+    //мои изменения
+    //создаём массив для хранения клиентов, которые зашли в чат (полный адрес)
+    static  ArrayList <ServerSocket> usersFull = new ArrayList <>();
+    //создаём массив для хранения клиентов, которые зашли в чат (имена)
+    static  ArrayList <String> users = new ArrayList <>();
+    //закончились мои изменения
     public static void main(String[] args) {
-        int port = 1777; //определяем номер порта, который будет "слушать" сервер
+        //определяем номер порта, который будет "слушать" сервер
+        int port = 1777;
         try {
             //открыть серверный сокет (ServerSocket)
             ServerSocket serverSocket = new ServerSocket(port);
@@ -15,12 +29,65 @@ public class ThreadServer {
             while (true) {
                 System.out.println("Ожидание связи с портом " + port);
                 // получив соединение начинаем работать с сокетом
-                Socket fromClientSocket = serverSocket.accept();
+                Socket fromClientSocket=serverSocket.accept();
                 // стартует новый поток для обработки запроса клиента
                 new SocketThread(fromClientSocket).start();
+                //добавляем нового пользователя в список тех, кто в чате
+                usersFull.add(serverSocket);
             }
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
     }
+
 }
+
+class SocketThread extends Thread {
+
+    public Socket fromClientSocket;
+    public SocketThread(Socket fromClientSocket) {
+
+        this.fromClientSocket=fromClientSocket;
+    }
+
+
+    @Override
+    public void run(){
+        //автоматически будут закрыты все ресурсы
+        try (
+                Socket localSocket = fromClientSocket;
+                PrintWriter pw = new PrintWriter(localSocket.getOutputStream(), true);
+                BufferedReader br=new BufferedReader(new InputStreamReader(localSocket.getInputStream())))
+        {
+            //читаем сообщения от клиента до тех пор пока он не скажет "Пока"
+
+            String messager;
+            //ура! тут научился видеть имя!!!!
+            String name=br.readLine();
+            ThreadServer.users.add(name);
+            Date date=new Date();
+            SimpleDateFormat R= new SimpleDateFormat("(HH:mm)");
+
+            while ((messager = br.readLine()) != null) {
+                // печатаем сообщение
+                for (ServerSocket serverSocket: ThreadServer.usersFull){///////////////////////////////////////////////////////////////
+                    pw.println(R.format(date) + "Сообщение от " + name + ": " + messager);
+                    System.out.println(R.format(date) + "Сообщение от " + name + ": " + messager);
+                    pw.flush();
+                }
+
+                //сравниваем с "Пока" и если это так, то выходим из цикла и закрываем соединение
+                if (messager.equals("Пока")) {
+                    // тоже говорим клиенту "Пока"
+                    pw.println("Пока");
+                    break;}
+                else  if (messager.equals("Кто в сети?")) {
+                    System.out.println(ThreadServer.users);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
